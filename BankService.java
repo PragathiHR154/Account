f98bb063038bab4e46f9ce28bcbb1a6c24fb6989
PragathiHR154 <pragathihr44@gmail.com>
2026-01-10 10:18:05 +0530

    BankService.java

---
+++ BankService.java
@@ -0,0 +1,42 @@
+class BankAccount {
+    private double balance;
+
+    // Constructor
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    // Deposit money
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    // Withdraw money
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    // Show balance
+    public void showBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+}
+
+public class BankService {
+    public static void main(String[] args) {
+        // Create account with initial balance
+        BankAccount account = new BankAccount(1000);
+
+        // Perform simple operations
+        account.showBalance();
+        account.deposit(500);
+        account.withdraw(200);
+        account.showBalance();
+    }
+}
